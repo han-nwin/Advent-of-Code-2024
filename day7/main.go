daeb37ff49479b7ca4f4758f217104ad91b20e4b
han-nwin <hannguyen.win@gmail.com>
2024-12-10 23:52:17 -0600

    part1

--- day7/main.go
+++ day7/main.go
@@ -19,7 +19,7 @@ func main () {
     lines := strings.Split(string(data), "\n") //Array of lines
     lines = lines[:len(lines)-1] //Cut the last empty line
     
-     
+    ans1 := 0
 
     for _, line := range lines {
 
@@ -27,37 +27,71 @@ func main () {
 
         // str_arr[0] is the result
         //slice the number out
-        numeric := str_arr[0]
-        result_hldr, _ := strconv.Atoi(numeric[:len(numeric)-1])
+        result_str := str_arr[0]
+        result_hldr, _ := strconv.Atoi(result_str[:len(result_str)-1])
         
         fmt.Println("Expected:",result_hldr)
 
         //NOTE: How to add operators in between?
         // Start from str_arr[1]
-        var builder strings.Builder 
+        number_list := str_arr[1:]
 
-        for _, num := range str_arr {
-            builder.WriteString(num)
 
-            //test operators
+        op_arr_list := arrange("*", "+", len(number_list) - 1)
 
+        for i := range op_arr_list {
+            var builder strings.Builder 
+            op_idx := 0
+
+            for _, num := range number_list{
+                builder.WriteString(num)
+                builder.WriteString(" ")
+                if op_idx < len(op_arr_list[i]) {
+                    builder.WriteString(string(op_arr_list[i][op_idx]))
+                    builder.WriteString(" ")
+                    op_idx++
+                }
+            }
+
+            new_str := strings.TrimSpace(builder.String())
+            new_str_arr := strings.Split(new_str, " ")
+            fmt.Println(new_str_arr)
+
+            calculation := execute(new_str_arr)
+            if calculation == result_hldr {
+                ans1 += calculation
+                break
+            }
+            fmt.Println("Calculation:", calculation)
         }
-
-        new_str := strings.TrimSpace(builder.String())
-        new_str_arr := strings.Split(new_str, " ")
-        fmt.Println("Calculation:",execute(new_str_arr))
     }
     
+    fmt.Println("Answer 1:", ans1)
 
 }
 
 //helper function to arrange operators all possible ways
 //return [] of operators string : "+ * + + *"
-func arrange (num_of_op int) []string {
-    for i := 0; i < num_of_op; i++ {
-        
-    } 
+func arrange (operator_1 string, operator_2 string, num_of_pos int) []string {
+    
+    num_of_arrage := 1 << num_of_pos // 2^num_of_pos
+    
+    arrangements := make([]string, 0, num_of_arrage)
+    for i := 0; i < num_of_arrage; i++ {
+         var builder strings.Builder
+        //mapping to get posision (1 << bit) = 001, 010, 100. (exp: num of pos = 3)
+         for bit := 0; bit < num_of_pos; bit++ {
+             if i & (1 << bit) != 0 {
+                builder.WriteString(operator_1)
+             } else {
+                 builder.WriteString(operator_2)
+             }
+         }
 
+        arrangements = append(arrangements, builder.String())
+    } 
+    
+    return arrangements
 }
 
 
