7ba11698c678989bc1a4f8684fa132aaf09f1fef
han-nwin <hannguyen.win@gmail.com>
2025-01-13 11:56:24 -0600

    s

--- day12/main.go
+++ day12/main.go
@@ -8,55 +8,59 @@ import (
 )
 
 type Plant struct {
-    kind rune
-    location [2]int
-    around int
+	kind     rune
+	location [2]int
+	neighbors int
 }
 
-//TODO: Implement get around
-func (p *Plant) getAround() {
-    p.around = 0
+// TODO:1. Implement get neighbor
+// to get number of the neighbor (neighbor = plant of the same kind)
+// we may not need this since we can just construct the neighbor when constructing []Plant (garden)
+func (p *Plant) getNeighbors() {
+	p.neighbors= 0
 }
 
 type Fence struct {
-    plant Plant
-    numOfPlants int
+	plant       Plant
+	numOfPlants int
 }
 
 func (f *Fence) area() int {
-    return f.numOfPlants
+	return f.numOfPlants
 }
-//TODO: Calculate perimeter
+
+// TODO: Calculate perimeter
 func (f *Fence) perimeter() int {
-    return 0
+	return 0
 }
 
 func main() {
-    if len(os.Args) < 2 {
-        log.Fatalln("input file name not provided")
-    }
+	if len(os.Args) < 2 {
+		log.Fatalln("input file name not provided")
+	}
 	data, err := os.ReadFile(os.Args[1])
 	if err != nil {
 		log.Fatalln("Cannot read input file")
 	}
 
-    lines := strings.Split(strings.TrimSpace(string(data)), "\n")
-    fmt.Println(lines)
+	lines := strings.Split(strings.TrimSpace(string(data)), "\n")
+	fmt.Println(lines)
 
-    var plants []Plant
+	var plants []Plant
 
-    for i, line := range lines {
-        for j, k := range line {
-            newPlant := Plant{
-                kind: k,
-                location: [2]int{i, j},
-            }
-            newPlant.getAround()
+	for i, line := range lines {
+		for j, k := range line {
+			newPlant := Plant{
+				kind:     k,
+				location: [2]int{i, j},
+			}
+            // TODO: 1. Construct neighbors manually here
+			newPlant.getNeighbors()
 
-            plants = append(plants, newPlant)
-        }
-    }
-    
-    fmt.Println(plants)
-    
+			plants = append(plants, newPlant)
+		}
+	}
+
+	fmt.Println(plants)
+
 }
